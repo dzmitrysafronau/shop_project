a4d30db7e92947bf965eff2b49c52ea39093e7b3
dzmitrysafronau <dmitry.safronov@capital.com>
2025-09-23 17:28:40 +0200

    price validation added

--- shop/tests/test_models.py
+++ shop/tests/test_models.py
@@ -8,3 +8,4 @@ def test_product_price_cannot_be_negative():
     product = Product(name="Bad", price=Decimal("-1"))
     with pytest.raises(ValidationError):
         product.full_clean()  # вызывает встроенные валидаторы модели
+        
